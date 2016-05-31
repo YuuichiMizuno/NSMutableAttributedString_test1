cee2bf9e07d1cbdfe604d37f063ce9f6a2d8eb11
yuuichi watanabe <yuuch_w@hotmail.com>
2016-05-31 11:52:39 +0900

    Initial Commit

---
+++ NSMutableAttributedString_test1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NSMutableAttributedString_test1
+//
+//  Created by yuichi.watanabe on 2016/05/31.
+//  Copyright © 2016年 yuichi.watanabe. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
